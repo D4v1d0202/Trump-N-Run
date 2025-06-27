f446b59eb4ea4379b2a62d275c25f69f073c10c9
dvowe <david.vowe@gmail.com>
2025-06-27 17:33:12 +0200

    added respawn option, small level adjustments, changed movement speed to 5

---
+++ Assets/Scripts/RespawnManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnManager : MonoBehaviour
+{
+    public Transform player;
+    public Transform playerStartPoint;
+    public Transform playerCheckpoint1;
+
+    private Transform currentRespawnPoint;
+
+    void Start()
+    {
+        currentRespawnPoint = playerStartPoint;
+    }
+
+    void Update()
+    {
+        if (player.position.x < playerCheckpoint1.position.x)
+        {
+            currentRespawnPoint = playerCheckpoint1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Rigidbody rb = player.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.velocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+            }
+
+            player.position = currentRespawnPoint.position;
+            player.rotation = currentRespawnPoint.rotation;
+        }
+    }
+}
