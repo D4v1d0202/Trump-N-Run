96cd7a0cef4212b9f59b771d3c3169f3f609fbbf
D4v1d0202 <dmlazzarini02@gmail.com>
2025-09-15 23:14:29 +0200

    more end

---
+++ Assets/Scripts/EndTriggerWithBomb.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTriggerWithBomb : MonoBehaviour
+{
+   public GameObject player;
+    public Camera mainCamera;
+    public Transform cameraTarget;
+    public AudioSource audioSource;
+    public AudioClip bombClip;
+    [Range(0f, 1f)] public float volume = 1f;
+
+    private PlayerMovement playerMovement;
+    private bool triggered = false;
+
+    void Start()
+    {
+        if (player != null)
+            playerMovement = player.GetComponent<PlayerMovement>();
+        if (mainCamera == null)
+            mainCamera = Camera.main;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (triggered || other.gameObject != player) return;
+        if (playerMovement == null || !playerMovement.GetGotBomb()) return;
+
+        triggered = true;
+
+        playerMovement.enabled = false;
+
+        if (mainCamera != null && cameraTarget != null)
+        {
+            mainCamera.transform.SetParent(null);
+            mainCamera.transform.position = cameraTarget.position;
+            mainCamera.transform.rotation = cameraTarget.rotation;
+        }
+
+        if (audioSource != null && bombClip != null)
+            audioSource.PlayOneShot(bombClip, volume);
+    }
+}
